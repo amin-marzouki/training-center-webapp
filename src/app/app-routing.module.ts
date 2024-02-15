111a918cd4644b94a338e1a2c824b16fe9c75059
amin-bit <genuiinfo@gmail.com>
2024-02-15 11:19:21 +0100

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StudentListComponent} from './components/student-list/student-list.component';
+import {AddStudentComponent} from "./components/add-student/add-student.component";
+import {StudentDetailComponent} from "./components/student-detail/student-detail.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',redirectTo:'tutorials',},
+  {path:'/student/:id',component:StudentDetailComponent},
+  {path:'/student-list',component:StudentListComponent},
+  {path:'/student-add',component:AddStudentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+import {FormsModule} from "@angular/forms";
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { StudentDetailComponent } from './components/student-detail/student-detail.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCompComponent,
+    AddStudentComponent,
+    StudentDetailComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
